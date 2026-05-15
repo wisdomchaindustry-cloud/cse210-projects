48d30ac9005734518c9e5904e26a084775d6ddf6
Francis <wisdomchaindustry@gmail.com>
2026-05-15 16:44:38 +0100

    creating classes in c sharp

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Google";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Francis Ime";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
