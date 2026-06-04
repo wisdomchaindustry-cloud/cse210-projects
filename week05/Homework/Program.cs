cb43e1d21d318db7de7969d6767715a942ee3e95
Francis <wisdomchaindustry@gmail.com>
2026-06-04 13:14:11 +0100

    Homework on Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment =
+            new MathAssignment(
+                "Roberto Rodriguez",
+                "Fractions",
+                "7.3",
+                "8-19");
+
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment =
+            new WritingAssignment(
+                "Mary Waters",
+                "European History",
+                "The Causes of World War II");
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
